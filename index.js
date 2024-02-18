f1610812cd9a5467b69888cf9d8148b7757a10bc
Tanmuch7583 <149760333+Tanmuch7583@users.noreply.github.com>
2024-02-18 06:38:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const center2 = document.querySelector('center2');
+const center5 = document.querySelector('center5');
+form.addEventListener('submit', (e) => {
+    e.preventdefault();
+}
+
+
+)
+
